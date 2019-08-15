944cecefafa02815fa40c62e3e3566c53c4d5b03
konoda <k.onoda.carret@gmail.com>
2019-08-16 01:49:23 +0900

    Sample livewire code commit

---
+++ resources/views/contents/livewire.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Livewire Sample</title>
+
+</head>
+<body>
+<h1>Livewire Sample</h1>
+<div>
+  @livewire('counter')
+</div>
+
+@livewireAssets
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/livewire', 'LivewireController@index')->name('livewire.index');
